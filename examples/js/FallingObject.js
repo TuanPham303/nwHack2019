6a7e691b117dbf3a78d64997a1fed91b446a11b3
TuanPham303 <phamcongtuan303@gmail.com>
2019-01-26 23:28:26 -0800

    add score

--- examples/js/FallingObject.js
+++ examples/js/FallingObject.js
@@ -1,17 +1,19 @@
 const objectCanvas = document.getElementById('objectCanvas');
+const pointCounter = document.getElementById('pointCounter');
 objectCanvas.width = window.innerWidth;
 objectCanvas.height = window.innerHeight;
 const ctx = objectCanvas.getContext('2d');
 
 const objectNumber = 5;
 const objects = [];
+let point = 0;
+let faceX, faceY, faceWidth, faceHeight;
 
 function object(){
   this.x = Math.round(Math.random() * objectCanvas.width);
   this.y = Math.round(Math.random() * objectCanvas.height);
   this.r = 30; 
   this.dy = 20;
-  // this.wind=Math.random()*5
 }
 
 object.prototype.update=function(){
@@ -26,6 +28,12 @@ object.prototype.update=function(){
      this.y = 0;
      this.x = Math.round(Math.random() * objectCanvas.width);
      this.dy++;
+  }
+  if (faceX < this.x && this.x < faceX + faceWidth && this.y >= faceY) {
+    point++;
+    pointCounter.innerHTML = point;
+    this.y = 0;
+    this.x = Math.round(Math.random() * objectCanvas.width);
   }
 }
 
@@ -61,13 +69,19 @@ window.onload = function(){
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     event.data.forEach(function(rect) {
-      context.strokeStyle = '#a64ceb';
-      context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-      context.font = '11px Helvetica';
-      context.fillStyle = "#fff";
-      context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
-      context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+      // context.strokeStyle = '#a64ceb';
+      // context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+      // context.font = '11px Helvetica';
+      // context.fillStyle = "#fff";
+      // context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+      // context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+      faceX = rect.x;
+      faceY = rect.y;
+      faceWidth = rect.width;
+      faceHeight = rect.height;
     });
   });
+
+
 }
 

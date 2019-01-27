4dec8aab4344384dc5393c55a0da6d97b9593c12
TuanPham303 <phamcongtuan303@gmail.com>
2019-01-26 22:40:25 -0800

    upload build

--- examples/js/FallingObject.js
+++ examples/js/FallingObject.js
@@ -10,7 +10,7 @@ function object(){
   this.x = Math.round(Math.random() * objectCanvas.width);
   this.y = Math.round(Math.random() * objectCanvas.height);
   this.r = 30; 
-  this.dy = 5;
+  this.dy = 20;
   // this.wind=Math.random()*5
 }
 
@@ -40,34 +40,34 @@ window.onload = function(){
   for(i = 0; i < objectNumber; i++){
      objects.push(new object())
   } 
-  setInterval(fallingObject, 50);
+  setInterval(fallingObject, 1);
 
-  // var video = document.getElementById('video');
-  // var canvas = document.getElementById('canvas');
+  var video = document.getElementById('video');
+  var canvas = document.getElementById('canvas');
 
-  // canvas.width = video.offsetWidth;
-  // canvas.height = video.offsetHeight;
+  canvas.width = video.offsetWidth;
+  canvas.height = video.offsetHeight;
 
-  // var context = canvas.getContext('2d');
+  var context = canvas.getContext('2d');
 
-  // var tracker = new tracking.ObjectTracker('face');
-  // tracker.setInitialScale(4);
-  // tracker.setStepSize(2);
-  // tracker.setEdgesDensity(0.1);
+  var tracker = new tracking.ObjectTracker('face');
+  tracker.setInitialScale(4);
+  tracker.setStepSize(2);
+  tracker.setEdgesDensity(0.1);
 
-  // tracking.track('#video', tracker, { camera: true });
+  tracking.track('#video', tracker, { camera: true });
 
-  // tracker.on('track', function(event) {
-  //   context.clearRect(0, 0, canvas.width, canvas.height);
+  tracker.on('track', function(event) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
-  //   event.data.forEach(function(rect) {
-  //     context.strokeStyle = '#a64ceb';
-  //     context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-  //     context.font = '11px Helvetica';
-  //     context.fillStyle = "#fff";
-  //     context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
-  //     context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
-  //   });
-  // });
+    event.data.forEach(function(rect) {
+      context.strokeStyle = '#a64ceb';
+      context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+      context.font = '11px Helvetica';
+      context.fillStyle = "#fff";
+      context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+      context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+    });
+  });
 }
 
